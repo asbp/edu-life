9ab60dded284f689d0cb83232abdd1eda7198677
zanuarts <nuartea27@gmail.com>
2020-08-23 14:09:51 +0700

    add register validation

--- lib/src/ui/pages/register/page_register.dart
+++ lib/src/ui/pages/register/page_register.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../constants/constant_main.dart';
 import '../../widgets/buttons/button_primary.dart';
 import '../login/page_login_home.dart';
-import '../page_main_layout.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -11,12 +10,33 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+  String _username = '';
+  String _email = '';
+  String _password = '';
+
   bool _obscureText = true;
   // Toggles the password show status
   void _toggle() {
     setState(() {
       _obscureText = !_obscureText;
     });
+  }
+
+  // dummy register 
+  void register(){
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print("form validate");
+    } else {
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+    print(_username);
+    print(_email);
+    print(_password);
   }
 
   @override
@@ -53,68 +73,14 @@ class _RegisterState extends State<RegisterPage> {
               ],
             ),
             SizedBox(height: 25),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: colorSecondary),
-                  borderRadius: BorderRadius.circular(40)),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20, right: 20),
-                    hintText: "username",
-                    labelText: "Username",
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none),
-              ),
+            Form(
+              key: _formKey,
+              autovalidate: _autoValidate,
+              child: formUI(),
             ),
+            
             SizedBox(height: 10),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: colorSecondary),
-                  borderRadius: BorderRadius.circular(40)),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20, right: 20),
-                    hintText: "email",
-                    labelText: "Email",
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: colorSecondary),
-                  borderRadius: BorderRadius.circular(40)),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20, right: 20),
-                    hintText: "password",
-                    labelText: "Password",
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    suffixIcon: IconButton(
-                        icon: Icon(_obscureText ? Icons.lock : Icons.lock_open),
-                        onPressed: () {
-                          _toggle();
-                        })),
-                obscureText: _obscureText,
-              ),
-            ),
+            
             SizedBox(height: 30),
             Container(
               child: Column(
@@ -122,10 +88,12 @@ class _RegisterState extends State<RegisterPage> {
                   ButtonPrimary(
                       text: "Daftar",
                       onClick: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeMainLayoutPage()));
+                        
+                        register();
+                        // Navigator.pushReplacement(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => HomeMainLayoutPage()));
                       }),
                   SizedBox(height: 10),
                   Row(
@@ -161,6 +129,117 @@ class _RegisterState extends State<RegisterPage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget formUI(){
+    return Column(
+      children: <Widget>[
+        Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: colorSecondary),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                // key: _formKey,
+                validator: (value) {
+                  if(value.length < 6){
+                    print('is empty');
+                    return 'Username must be more than 6 charater';
+                  }
+                    
+                  else
+                    return null;
+                },
+                onSaved: (String val) {
+                  _username = val;
+                },
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 20, right: 20),
+                    hintText: "username",
+                    labelText: "Username",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: colorSecondary),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextFormField(
+                // key: _formKey,
+                keyboardType: TextInputType.emailAddress,
+                validator: (String val) {
+                  Pattern pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val))
+                    return 'Enter Valid Email';
+                  else
+                    return null;
+                },
+                onSaved: (String val) {
+                  _email = val;
+                },           
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 20, right: 20),
+                    hintText: "email",
+                    labelText: "Email",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+             margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: colorSecondary),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextFormField(
+                // key: _formKey,
+                keyboardType: TextInputType.text,
+                validator: (String val) {
+                  if(val.length < 8)
+                    return 'Password must be more than 8 charater';
+                  else
+                    return null;
+                },
+                onSaved: (String val) {
+                  _username = val;
+                },
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 20, right: 20),
+                    hintText: "password",
+                    labelText: "Password",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    suffixIcon: IconButton(
+                        icon: Icon(_obscureText ? Icons.lock : Icons.lock_open),
+                        onPressed: () {
+                          _toggle();
+                        })),
+                obscureText: _obscureText,
+              ),
+            ),
+      ],
     );
   }
 }
